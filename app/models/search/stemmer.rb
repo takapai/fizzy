0048915f439f5e9f270dab276dce8a967da8bcb4
Takahisa Hasegawa <taka@paloaltoinsight.com>
2025-12-31 12:19:35 +0900

    Query and stemmer considers CJK

--- app/models/search/stemmer.rb
+++ app/models/search/stemmer.rb
@@ -5,9 +5,25 @@ module Search::Stemmer
 
   def stem(value)
     if value.present?
-      value.gsub(/[^\w\s]/, "").split(/\s+/).map { |word| STEMMER.stem(word.downcase) }.join(" ")
+      value.split(/\s+/).filter_map do |word|
+        if contains_cjk?(word)
+          word
+        else
+          cleaned_word = word.gsub(/[^\w\s]/, "")
+          if cleaned_word.present?
+            STEMMER.stem(cleaned_word.downcase)
+          else
+            nil
+          end
+        end
+      end.join(" ")
     else
       value
     end
   end
+
+  private
+    def contains_cjk?(text)
+      text.match?(/\p{Han}|\p{Hiragana}|\p{Katakana}/)
+    end
 end
